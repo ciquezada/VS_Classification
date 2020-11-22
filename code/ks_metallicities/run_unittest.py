3b10cf23d13131af402f10fb772df55fc6967708
Karlos <ciquezada@uc.cl>
2020-11-22 02:49:35 -0300

    added ouput test
    there is a inconsistency on output content (dont know the reason yet)

--- code/ks_metallicities/run_unittest.py
+++ code/ks_metallicities/run_unittest.py
@@ -1,4 +1,5 @@
 import os
+import io
 import unittest
 
 class pyfinerAdapterTestCase(unittest.TestCase):
@@ -10,13 +11,21 @@ class pyfinerAdapterTestCase(unittest.TestCase):
         os.system(test_string)
 
     def test_output_pdf_files(self):
-        n_pdfs = len([name for name in os.listdir('.')
-                                        if os.path.isfile("output_pdf")])
-        self.assertEqual(b_pdfs, 5, 'incorrect quantity of pdfs generated')
+        n_pdfs = len([name for name in os.listdir('output_pdf')
+                                        if os.path.isfile("output_pdf"+os.sep+name)])
+        self.assertEqual(n_pdfs, 5, 'incorrect quantity of pdfs generated')
 
-    def test_output_pdf_files(self):
+    def test_output_file(self):
         self.assertEqual(os.path.exists("test" + os.sep + "test_output.csv"),
                                                      True, 'output not created')
+
+    def test_output_content(self):
+        test_path = "test" + os.sep + "test_output.csv"
+        correct_test_path = "test" + os.sep + "correct_output.csv"
+        with io.open(test_path) as test:
+            with io.open(correct_test_path) as correct:
+                self.assertListEqual(list(test), list(correct),
+                                                            "incorrect output")
 
     def tearDown(self):
         os.system('rm -r output_pdf')
@@ -24,20 +33,28 @@ class pyfinerAdapterTestCase(unittest.TestCase):
 
 class pyfinerAdapterParallelTestCase(unittest.TestCase):
     def setUp(self):
-        test_string = "python adapter_Ks_metallicity.py 2 "
+        test_string = "python adapter_Ks_metallicity.py 3 "
         test_string += "test" + os.sep + "test_data "
         test_string += "test" + os.sep + "test_curves.csv "
         test_string += "test" + os.sep + "test_output.csv "
         os.system(test_string)
 
     def test_output_pdf_files(self):
-        n_pdfs = len([name for name in os.listdir('.')
-                                        if os.path.isfile("output_pdf")])
-        self.assertEqual(b_pdfs, 5, 'incorrect quantity of pdfs generated')
+        n_pdfs = len([name for name in os.listdir('output_pdf')
+                                if os.path.isfile("output_pdf"+os.sep+name)])
+        self.assertEqual(n_pdfs, 5, 'incorrect quantity of pdfs generated')
 
-    def test_output_pdf_files(self):
+    def test_output_file(self):
         self.assertEqual(os.path.exists("test" + os.sep + "test_output.csv"),
                                                      True, 'output not created')
+
+    def test_output_content(self):
+        test_path = "test" + os.sep + "test_output.csv"
+        correct_test_path = "test" + os.sep + "correct_output.csv"
+        with io.open(test_path) as test:
+            with io.open(correct_test_path) as correct:
+                self.assertListEqual(list(test), list(correct),
+                                                            "incorrect output")
 
     def tearDown(self):
         os.system('rm -r output_pdf')
